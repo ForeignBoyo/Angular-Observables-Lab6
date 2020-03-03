12012adfe136694bc6300f12316049fef89b7191
KRYSTIAN OPRYSZEK - STUDENT <G00366895@gmit.ie>
2020-03-03 17:47:33 +0000

    Lab6

---
+++ src/app/Services/data.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http:HttpClient) { }
+
+  GetStudentData():Observable<any>{
+    return this.http.get('https://www.jsonblob.com/api/jsonblob/0c311081-5d6a-11ea-80ec-fb39591bf51c');
+  }
+
+  GetWeatherData():Observable<any>{
+    return this.http.get('http://api.openweathermap.org/data/2.5/weather?q=Galway&appid=6a66416403ed8e5e6e762cb8c261f303');
+  }
+}
